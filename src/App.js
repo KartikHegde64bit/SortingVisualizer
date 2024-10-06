56e85ae60b0f38a4a9165d4ab7d18e617638e189
KartikHegde64bit <kartikhegde062@gmail.com>
2024-10-06 17:42:48 +0530

    Bar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+import Bar from './components/Bar';
+
+import './components/Bar.css'
+import './App.css';
+class App extends Component {
+  	state = {
+		array: [],
+		arraySteps: [],
+		colorKey: [],
+		colorSteps: [],
+		currentStep: 0,
+		count: 10,
+		delay: 100,
+		algorithm: '',
+		timeouts: [],
+	};
+
+componentDidMount(){
+	this.generteRandomArray();
+}
+
+	generateRandomNumber = (min, max) => {
+		return Math.floor(Math.random() * (max-min) + min);
+	}
+
+	generteRandomArray = () => {
+		const count = this.state.count;
+		const temp = this.state.array;
+
+		for(let i=0; i < count; i++) {
+			temp.push(this.generateRandomNumber(50, 200));
+		}
+
+		this.setState({
+			array: temp,
+			arraySteps: [temp]
+		});
+	}
+
+	render() {
+		let bars = this.state.array.map((value, index)=>{
+			<Bar 
+				key={index} 
+				index={index} 
+				length={value} 
+				color={this.state.colorKey[index]}/>
+		});
+		return <div className='app'>
+			<h1>{bars}</h1>
+		</div>
+	}
 }
 
 export default App;
---
+++ src/components/Bar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Bar.css'
+
+function Bar() {
+  return (
+    <>
+        <div className='bar'>
+
+        </div>
+    </>
+  )
+}
+
+export default Bar
