516660cdacce7fc238559e46dd3b1f1336f89093
KartikHegde64bit <kartikhegde062@gmail.com>
2024-10-07 22:24:49 +0530

    issue fixes

--- src/algorithms/BS.js
+++ src/algorithms/BS.js
@@ -1,13 +1,13 @@
-import {swap} from './helpers.js';
+import {swap} from './helpers';
 
 const bs = (array, position, arraySteps, colorSteps) => {
-    let colorKey = colorSteps[colorSteps.length - 1].slice();
+	let colorKey = colorSteps[colorSteps.length - 1].slice();
 
-    for(let i=0; i < array.length - 1; i++) {
-        for(let j=0; j < array - i - 1; j++) {
-            if(array[j] > array[j+1]) {
-                array = swap(array, j, j+1);
-            }     
+	for (let i = 0; i < array.length - 1; i++) {
+		for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                array = swap(array, j, j + 1);
+            }
             arraySteps.push(array.slice());
             colorKey[j] = 1;
             colorKey[j + 1] = 1;
@@ -15,12 +15,12 @@ const bs = (array, position, arraySteps, colorSteps) => {
             colorKey[j] = 0;
             colorKey[j + 1] = 0;
         }
-        colorKey[array.length-1-i] = 2;
+        colorKey[arraySteps.length - 1 - i] = 2;
         arraySteps.push(array.slice());
         colorSteps.push(colorKey.slice());
-    }
+	}
     colorSteps[colorSteps.length - 1] = new Array(array.length).fill(2);
     return;
-}
+};
 
 export default bs;
