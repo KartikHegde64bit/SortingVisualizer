6cc2fc6aa21505823d9d20eb45798e5c80cca7b6
kartik.hegde <kartik.hegde@zohocorp.com>
2026-02-27 23:55:35 +0530

    refactor: update App component structure and styling for improved theme management

--- src/App.js
+++ src/App.js
@@ -5,11 +5,34 @@ import './App.css';
 import HomePage from './components/HomePage/HomePage';
 
 class App extends Component {
-	
+	state = {
+		theme: 'light',
+	};
+
+	componentDidMount() {
+		this.applyTheme();
+	}
+
+	componentDidUpdate(_, prevState) {
+		if (prevState.theme !== this.state.theme) {
+			this.applyTheme();
+		}
+	}
+
+	applyTheme = () => {
+		document.documentElement.setAttribute('data-theme', this.state.theme);
+	};
+
+	toggleTheme = () => {
+		this.setState((prev) => ({ theme: prev.theme === 'light' ? 'dark' : 'light' }));
+	};
+
 	render() {
 		return (
-		<div><HomePage/></div> 
-		)
+			<div className="app-shell">
+				<HomePage theme={this.state.theme} onToggleTheme={this.toggleTheme} />
+			</div>
+		);
 	}
 }
 
